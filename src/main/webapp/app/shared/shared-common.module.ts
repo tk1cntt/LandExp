52ca283548bbeeb075d100deff0c56bf1fc35bb2
Tran Anh Tuan <tk1cntt@gmail.com>
2018-06-07 06:52:20 +0700

    Initial application generated by JHipster-4.14.0

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+
+import {
+    LandexpSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        LandexpSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'en'
+        },
+    ],
+    exports: [
+        LandexpSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class LandexpSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
