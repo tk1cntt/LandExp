c6235f4dc1a7fcadca75e30b9153017a301a3738
Tran Anh Tuan <tk1cntt@gmail.com>
2018-06-12 21:05:05 +0700

    Add step two

--- src/main/webapp/app/modules/post/stepTwo.tsx
+++ src/main/webapp/app/modules/post/stepTwo.tsx
@@ -5,26 +5,90 @@ import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
 import { Row, Col, Alert } from 'reactstrap';
+import { Cascader, Input } from 'antd';
 
 import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
 
 export interface IStepTwoProp extends StateProps, DispatchProps {}
 
+const options = [{
+  value: 'HANOI',
+  label: 'Hà Nội',
+  children: [{
+    value: 'CAUGIAY',
+    label: 'Cầu Giấy',
+    children: [{
+      value: 'DICHVONG',
+      label: 'Dịch Vọng'
+    }, {
+      value: 'DICHVONGHAU',
+      label: 'Dịch Vọng Hậu'
+    }, {
+      value: 'TRUNGHOA',
+      label: 'Trung Hoà'
+    }, {
+      value: 'YENHOA',
+      label: 'Yên Hoà'
+    }]
+  }, {
+    value: 'HADONG',
+    label: 'Hà Đông',
+    children: [{
+      value: 'VANPHUC',
+      label: 'Vạn Phúc'
+    }, {
+      value: 'VANQUAN',
+      label: 'Văn Quán'
+    }]
+  }]
+}, {
+  value: 'HOCHIMINH',
+  label: 'Hồ Chí Minh',
+  children: [{
+    value: 'QUAN1',
+    label: 'Quận 1',
+    children: [{
+      value: 'BENNGHE',
+      label: 'Bến Nghé'
+    }, {
+      value: 'BENTHANH',
+      label: 'Bến Nghé'
+    }]
+  }, {
+    value: 'QUAN2',
+    label: 'Quận 2',
+    children: [{
+      value: 'ANKHANH',
+      label: 'An Khánh'
+    }, {
+      value: 'BINHKHANH',
+      label: 'Bình Khánh'
+    }]
+  }]
+}];
+
 export class StepTwo extends React.Component<IStepTwoProp> {
   componentDidMount() {
     this.props.getSession();
+  }
+
+  onChange = (e) => {
+    console.log('Cascader changed', e);
   }
 
   render() {
     const { account } = this.props;
     return (
       <Row>
-        <Col md="6">
-          <h6>Step Two</h2>
+        <Col md="12">
+          <h5>Vị trí bất động sản của bạn</h5>
         </Col>
-        <Col md="6" className="pad">
-          <h6>Menu left</h2>
+        <Col md="12">
+          <Cascader style={{ width: '50%' }} options={options} onChange={this.onChange} placeholder="Please select" />
+          <div style={{ marginTop: 16 }}>
+            <Input addonBefore="Địa chỉ chi tiết" placeholder="Số nhà, ngõ, ngách, phố"/>
+          </div>
         </Col>
       </Row>
     );
