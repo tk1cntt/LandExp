ed0281cd7cc05aa2611a036205f3f2e60ee6b67b
Tran Anh Tuan <tk1cntt@gmail.com>
2018-07-11 06:53:14 +0700

    Update

--- src/main/webapp/app/modules/account/house/house.tsx
+++ src/main/webapp/app/modules/account/house/house.tsx
@@ -100,100 +100,108 @@ export class House extends React.Component<IHouseProps, IHouseState> {
       <Row>
         <SearchPage location={this.props.location} history={this.props.history} />
         <Container>
-          <Spin spinning={this.props.loading} tip="Đang cập nhật dữ liệu...">
-            <h2 id="house-heading">
-              <Translate contentKey="landexpApp.house.home.title">Houses</Translate>
-            </h2>
-            <div className="table-responsive">
-              <Table responsive>
-                <thead>
-                  <tr>
-                    <th className="hand" onClick={this.sort('actionType')}>
-                      <Translate contentKey="landexpApp.house.actionType">Action Type</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th className="hand" onClick={this.sort('landType')}>
-                      <Translate contentKey="landexpApp.house.landType">Land Type</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th className="hand" onClick={this.sort('money')}>
-                      <Translate contentKey="landexpApp.house.money">Money</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th className="hand" onClick={this.sort('discount')}>
-                      <Translate contentKey="landexpApp.house.discount">Discount</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th>
-                      <Translate contentKey="landexpApp.house.city">City</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th>
-                      <Translate contentKey="landexpApp.house.district">District</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th>
-                      <Translate contentKey="landexpApp.house.ward">Ward</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th>
-                      <Translate contentKey="landexpApp.house.saleType">Sale Type</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th>
-                      <Translate contentKey="landexpApp.house.statusType">Status Type</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th>
-                      <Translate contentKey="landexpApp.house.createBy">Create By</Translate> <FontAwesomeIcon icon="sort" />
-                    </th>
-                    <th />
-                  </tr>
-                </thead>
-                <tbody>
-                  {houseList.map((house, i) => (
-                    <tr key={`entity-${i}`}>
-                      <td>{house.actionType === 'FOR_SELL' ? 'Bán' : 'Cho thuê'}</td>
-                      <td>{getLandType(house.landType)}</td>
-                      <td>{new Intl.NumberFormat().format(house.money)} VNĐ</td>
-                      <td>{new Intl.NumberFormat().format(house.discount)} VNĐ</td>
-                      <td>{house.cityName ? <Link to={`city/${house.cityId}`}>{house.cityName}</Link> : ''}</td>
-                      <td>{house.districtName ? <Link to={`district/${house.districtId}`}>{house.districtName}</Link> : ''}</td>
-                      <td>{house.wardName ? <Link to={`ward/${house.wardId}`}>{house.wardName}</Link> : ''}</td>
-                      <td>{getSaleType(house.saleType)}</td>
-                      <td>{getStatusType(house.statusType)}</td>
-                      <td>{house.createByLogin}</td>
-                      <td className="text-right">
-                        <div className="btn-group flex-btn-group-container">
-                          <Button tag={Link} to={`/tai-khoan/xem-truoc-tin-dang/${encodeId(house.id)}`} color="info" size="sm">
-                            <FontAwesomeIcon icon="eye" />{' '}
-                            <span className="d-none d-md-inline">
-                              <Translate contentKey="entity.action.view">View</Translate>
-                            </span>
-                          </Button>
-                          <Button tag={Link} to={`/tai-khoan/sua-tin-dang/${encodeId(house.id)}`} color="primary" size="sm">
-                            <FontAwesomeIcon icon="pencil-alt" />{' '}
-                            <span className="d-none d-md-inline">
-                              <Translate contentKey="entity.action.edit">Edit</Translate>
-                            </span>
-                          </Button>
-                          {house.statusType === 'PENDING' ? (
-                            <Button tag={Link} to={`/tai-khoan/thanh-toan/${encodeId(house.id)}`} color="warning" size="sm">
-                              <FontAwesomeIcon icon="coffee" />{' '}
-                              <span className="d-none d-md-inline">
-                                <Translate contentKey="entity.action.pay">Pay</Translate>
-                              </span>
-                            </Button>
-                          ) : (
-                            ''
-                          )}
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
-            <Row className="justify-content-center">
-              <JhiPagination
-                items={getPaginationItemsNumber(totalItems, this.state.itemsPerPage)}
-                activePage={this.state.activePage}
-                onSelect={this.handlePagination}
-                maxButtons={5}
-              />
-            </Row>
-          </Spin>
+          <Col md="12">
+            {this.props.loading ? (
+              <div className="justify-content-center">
+                <Spin tip="Đang cập nhật dữ liệu..." />
+              </div>
+            ) : (
+              <>
+                <h2>
+                  <Translate contentKey="landexpApp.house.home.title">Houses</Translate>
+                </h2>{' '}
+                <div>
+                  <Table responsive>
+                    <thead>
+                      <tr>
+                        <th className="hand" onClick={this.sort('actionType')}>
+                          <Translate contentKey="landexpApp.house.actionType">Action Type</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th className="hand" onClick={this.sort('landType')}>
+                          <Translate contentKey="landexpApp.house.landType">Land Type</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th className="hand" onClick={this.sort('money')}>
+                          <Translate contentKey="landexpApp.house.money">Money</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th className="hand" onClick={this.sort('discount')}>
+                          <Translate contentKey="landexpApp.house.discount">Discount</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th>
+                          <Translate contentKey="landexpApp.house.city">City</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th>
+                          <Translate contentKey="landexpApp.house.district">District</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th>
+                          <Translate contentKey="landexpApp.house.ward">Ward</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th>
+                          <Translate contentKey="landexpApp.house.saleType">Sale Type</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th>
+                          <Translate contentKey="landexpApp.house.statusType">Status Type</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th>
+                          <Translate contentKey="landexpApp.house.createBy">Create By</Translate> <FontAwesomeIcon icon="sort" />
+                        </th>
+                        <th />
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {houseList.map((house, i) => (
+                        <tr key={`entity-${i}`}>
+                          <td>{house.actionType === 'FOR_SELL' ? 'Bán' : 'Cho thuê'}</td>
+                          <td>{getLandType(house.landType)}</td>
+                          <td>{new Intl.NumberFormat().format(house.money)} VNĐ</td>
+                          <td>{new Intl.NumberFormat().format(house.discount)} VNĐ</td>
+                          <td>{house.cityName ? <Link to={`city/${house.cityId}`}>{house.cityName}</Link> : ''}</td>
+                          <td>{house.districtName ? <Link to={`district/${house.districtId}`}>{house.districtName}</Link> : ''}</td>
+                          <td>{house.wardName ? <Link to={`ward/${house.wardId}`}>{house.wardName}</Link> : ''}</td>
+                          <td>{getSaleType(house.saleType)}</td>
+                          <td>{getStatusType(house.statusType)}</td>
+                          <td>{house.createByLogin}</td>
+                          <td className="text-right">
+                            <div className="btn-group flex-btn-group-container">
+                              <Button tag={Link} to={`/tai-khoan/xem-truoc-tin-dang/${encodeId(house.id)}`} color="info" size="sm">
+                                <FontAwesomeIcon icon="eye" />{' '}
+                                <span className="d-none d-md-inline">
+                                  <Translate contentKey="entity.action.view">View</Translate>
+                                </span>
+                              </Button>
+                              <Button tag={Link} to={`/tai-khoan/sua-tin-dang/${encodeId(house.id)}`} color="primary" size="sm">
+                                <FontAwesomeIcon icon="pencil-alt" />{' '}
+                                <span className="d-none d-md-inline">
+                                  <Translate contentKey="entity.action.edit">Edit</Translate>
+                                </span>
+                              </Button>
+                              {house.statusType === 'PENDING' ? (
+                                <Button tag={Link} to={`/tai-khoan/thanh-toan/${encodeId(house.id)}`} color="warning" size="sm">
+                                  <FontAwesomeIcon icon="coffee" />{' '}
+                                  <span className="d-none d-md-inline">
+                                    <Translate contentKey="entity.action.pay">Pay</Translate>
+                                  </span>
+                                </Button>
+                              ) : (
+                                ''
+                              )}
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </div>
+                <Row className="justify-content-center">
+                  <JhiPagination
+                    items={getPaginationItemsNumber(totalItems, this.state.itemsPerPage)}
+                    activePage={this.state.activePage}
+                    onSelect={this.handlePagination}
+                    maxButtons={5}
+                  />
+                </Row>
+              </>
+            )}
+          </Col>
         </Container>
       </Row>
     );
--- src/main/webapp/app/modules/account/user-management/user-management.tsx
+++ src/main/webapp/app/modules/account/user-management/user-management.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { Button, Table, Row, Container, Badge } from 'reactstrap';
+import { Button, Table, Row, Col, Container, Badge } from 'reactstrap';
 import {
   Translate,
   ICrudGetAllAction,
@@ -13,6 +13,8 @@ import {
   IPaginationBaseState
 } from 'react-jhipster';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
+import { Spin } from 'antd';
 
 import { APP_DATE_FORMAT } from 'app/config/constants';
 import { ITEMS_PER_PAGE } from 'app/shared/util/pagination.constants';
@@ -67,119 +69,126 @@ export class UserManagement extends React.Component<IUserManagementProps, IPagin
       <Row>
         <SearchPage location={this.props.location} history={this.props.history} />
         <Container>
-          <h2 className="userManagement-page-heading">
-            <Translate contentKey="userManagement.home.title">Users</Translate>
-            <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity">
-              <FontAwesomeIcon icon="plus" /> <Translate contentKey="userManagement.home.createLabel">Create a new user</Translate>
-            </Link>
-          </h2>
-          <Table responsive striped>
-            <thead>
-              <tr>
-                <th className="hand" onClick={this.sort('login')}>
-                  <Translate contentKey="userManagement.login">Login</Translate>
-                  <FontAwesomeIcon icon="sort" />
-                </th>
-                <th className="hand" onClick={this.sort('email')}>
-                  <Translate contentKey="userManagement.email">Email</Translate>
-                  <FontAwesomeIcon icon="sort" />
-                </th>
-                <th />
-                <th className="hand" onClick={this.sort('langKey')}>
-                  <Translate contentKey="userManagement.langKey">Lang Key</Translate>
-                  <FontAwesomeIcon icon="sort" />
-                </th>
-                <th>
-                  <Translate contentKey="userManagement.profiles">Profiles</Translate>
-                </th>
-                <th className="hand" onClick={this.sort('createdDate')}>
-                  <Translate contentKey="userManagement.createdDate">Created Date</Translate>
-                  <FontAwesomeIcon icon="sort" />
-                </th>
-                <th className="hand" onClick={this.sort('lastModifiedBy')}>
-                  <Translate contentKey="userManagement.lastModifiedBy">Last Modified By</Translate>
-                  <FontAwesomeIcon icon="sort" />
-                </th>
-                <th className="hand" onClick={this.sort('lastModifiedDate')}>
-                  <Translate contentKey="userManagement.lastModifiedDate">Last Modified Date</Translate>
-                  <FontAwesomeIcon icon="sort" />
-                </th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, i) => (
-                <tr id={user.login} key={`user-${i}`}>
-                  <td>{user.login}</td>
-                  <td>{user.email}</td>
-                  <td>
-                    {user.activated ? (
-                      <Button color="success" onClick={this.toggleActive(user)}>
-                        Activated
-                      </Button>
-                    ) : (
-                      <Button color="danger" onClick={this.toggleActive(user)}>
-                        Deactivated
-                      </Button>
-                    )}
-                  </td>
-                  <td>{user.langKey}</td>
-                  <td>
-                    {user.authorities
-                      ? user.authorities.map((authority, j) => (
-                          <div key={`user-auth-${i}-${j}`}>
-                            <Badge color="info">{authority}</Badge>
+          <Col md="12">
+            {this.props.loading ? (
+              <div className="justify-content-center">
+                <Spin tip="Đang cập nhật dữ liệu..." />
+              </div>
+            ) : (
+              <>
+                <h2 className="userManagement-page-heading">
+                  <Translate contentKey="userManagement.home.title">Users</Translate>
+                </h2>
+                <Table responsive striped>
+                  <thead>
+                    <tr>
+                      <th className="hand" onClick={this.sort('login')}>
+                        <Translate contentKey="userManagement.login">Login</Translate>
+                        <FontAwesomeIcon icon="sort" />
+                      </th>
+                      <th className="hand" onClick={this.sort('email')}>
+                        <Translate contentKey="userManagement.email">Email</Translate>
+                        <FontAwesomeIcon icon="sort" />
+                      </th>
+                      <th />
+                      <th className="hand" onClick={this.sort('langKey')}>
+                        <Translate contentKey="userManagement.langKey">Lang Key</Translate>
+                        <FontAwesomeIcon icon="sort" />
+                      </th>
+                      <th>
+                        <Translate contentKey="userManagement.profiles">Profiles</Translate>
+                      </th>
+                      <th className="hand" onClick={this.sort('createdDate')}>
+                        <Translate contentKey="userManagement.createdDate">Created Date</Translate>
+                        <FontAwesomeIcon icon="sort" />
+                      </th>
+                      <th className="hand" onClick={this.sort('lastModifiedBy')}>
+                        <Translate contentKey="userManagement.lastModifiedBy">Last Modified By</Translate>
+                        <FontAwesomeIcon icon="sort" />
+                      </th>
+                      <th className="hand" onClick={this.sort('lastModifiedDate')}>
+                        <Translate contentKey="userManagement.lastModifiedDate">Last Modified Date</Translate>
+                        <FontAwesomeIcon icon="sort" />
+                      </th>
+                      <th />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {users.map((user, i) => (
+                      <tr id={user.login} key={`user-${i}`}>
+                        <td>{user.login}</td>
+                        <td>{user.email}</td>
+                        <td>
+                          {user.activated ? (
+                            <Button color="success" onClick={this.toggleActive(user)}>
+                              Activated
+                            </Button>
+                          ) : (
+                            <Button color="danger" onClick={this.toggleActive(user)}>
+                              Deactivated
+                            </Button>
+                          )}
+                        </td>
+                        <td>{user.langKey}</td>
+                        <td>
+                          {user.authorities
+                            ? user.authorities.map((authority, j) => (
+                                <div key={`user-auth-${i}-${j}`}>
+                                  <Badge color="info">{authority}</Badge>
+                                </div>
+                              ))
+                            : null}
+                        </td>
+                        <td>
+                          <TextFormat value={user.createdDate} type="date" format={APP_DATE_FORMAT} blankOnInvalid />
+                        </td>
+                        <td>{user.lastModifiedBy}</td>
+                        <td>
+                          <TextFormat value={user.lastModifiedDate} type="date" format={APP_DATE_FORMAT} blankOnInvalid />
+                        </td>
+                        <td className="text-right">
+                          <div className="btn-group flex-btn-group-container">
+                            <Button tag={Link} to={`${match.url}/${user.login}`} color="info" size="sm">
+                              <FontAwesomeIcon icon="eye" />{' '}
+                              <span className="d-none d-md-inline">
+                                <Translate contentKey="entity.action.view">View</Translate>
+                              </span>
+                            </Button>
+                            <Button tag={Link} to={`${match.url}/${user.login}/edit`} color="primary" size="sm">
+                              <FontAwesomeIcon icon="pencil-alt" />{' '}
+                              <span className="d-none d-md-inline">
+                                <Translate contentKey="entity.action.edit">Edit</Translate>
+                              </span>
+                            </Button>
+                            <Button
+                              tag={Link}
+                              to={`${match.url}/${user.login}/delete`}
+                              color="danger"
+                              size="sm"
+                              disabled={account.login === user.login}
+                            >
+                              <FontAwesomeIcon icon="trash" />{' '}
+                              <span className="d-none d-md-inline">
+                                <Translate contentKey="entity.action.delete">Delete</Translate>
+                              </span>
+                            </Button>
                           </div>
-                        ))
-                      : null}
-                  </td>
-                  <td>
-                    <TextFormat value={user.createdDate} type="date" format={APP_DATE_FORMAT} blankOnInvalid />
-                  </td>
-                  <td>{user.lastModifiedBy}</td>
-                  <td>
-                    <TextFormat value={user.lastModifiedDate} type="date" format={APP_DATE_FORMAT} blankOnInvalid />
-                  </td>
-                  <td className="text-right">
-                    <div className="btn-group flex-btn-group-container">
-                      <Button tag={Link} to={`${match.url}/${user.login}`} color="info" size="sm">
-                        <FontAwesomeIcon icon="eye" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.view">View</Translate>
-                        </span>
-                      </Button>
-                      <Button tag={Link} to={`${match.url}/${user.login}/edit`} color="primary" size="sm">
-                        <FontAwesomeIcon icon="pencil-alt" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.edit">Edit</Translate>
-                        </span>
-                      </Button>
-                      <Button
-                        tag={Link}
-                        to={`${match.url}/${user.login}/delete`}
-                        color="danger"
-                        size="sm"
-                        disabled={account.login === user.login}
-                      >
-                        <FontAwesomeIcon icon="trash" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.delete">Delete</Translate>
-                        </span>
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Row className="justify-content-center">
-            <JhiPagination
-              items={getPaginationItemsNumber(totalItems, this.state.itemsPerPage)}
-              activePage={this.state.activePage}
-              onSelect={this.handlePagination}
-              maxButtons={5}
-            />
-          </Row>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+                <Row className="justify-content-center">
+                  <JhiPagination
+                    items={getPaginationItemsNumber(totalItems, this.state.itemsPerPage)}
+                    activePage={this.state.activePage}
+                    onSelect={this.handlePagination}
+                    maxButtons={5}
+                  />
+                </Row>
+              </>
+            )}
+          </Col>
         </Container>
       </Row>
     );
@@ -188,6 +197,7 @@ export class UserManagement extends React.Component<IUserManagementProps, IPagin
 
 const mapStateToProps = (storeState: IRootState) => ({
   users: storeState.userManagement.users,
+  loading: storeState.userManagement.loading,
   totalItems: storeState.userManagement.totalItems,
   account: storeState.authentication.account
 });
