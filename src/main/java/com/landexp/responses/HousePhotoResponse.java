c2124d7542b5815b35585cb7abaef5d7b00cd648
Tran Anh Tuan <tuanta17@fsoft.com.vn>
2018-07-16 16:40:49 +0700

    Update

---
+++ src/main/java/com/landexp/responses/HousePhotoResponse.java
@@ -0,0 +1,170 @@
+package com.landexp.service.dto;
+
+import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Lob;
+
+/**
+ * A DTO for the HousePhoto entity.
+ */
+public class HousePhotoDTO implements Serializable {
+
+    private Long id;
+
+    @Lob
+    private byte[] image;
+    private String imageContentType;
+
+    private String mobileLink;
+
+    private String webLink;
+
+    private Boolean enabled;
+
+    private LocalDate createAt;
+
+    private Long houseId;
+
+    private Long createById;
+
+    private String createByLogin;
+
+    private Long updateById;
+
+    private String updateByLogin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getImageContentType() {
+        return imageContentType;
+    }
+
+    public void setImageContentType(String imageContentType) {
+        this.imageContentType = imageContentType;
+    }
+
+    public String getMobileLink() {
+        return mobileLink;
+    }
+
+    public void setMobileLink(String mobileLink) {
+        this.mobileLink = mobileLink;
+    }
+
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public Long getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(Long houseId) {
+        this.houseId = houseId;
+    }
+
+    public Long getCreateById() {
+        return createById;
+    }
+
+    public void setCreateById(Long userId) {
+        this.createById = userId;
+    }
+
+    public String getCreateByLogin() {
+        return createByLogin;
+    }
+
+    public void setCreateByLogin(String userLogin) {
+        this.createByLogin = userLogin;
+    }
+
+    public Long getUpdateById() {
+        return updateById;
+    }
+
+    public void setUpdateById(Long userId) {
+        this.updateById = userId;
+    }
+
+    public String getUpdateByLogin() {
+        return updateByLogin;
+    }
+
+    public void setUpdateByLogin(String userLogin) {
+        this.updateByLogin = userLogin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        HousePhotoDTO housePhotoDTO = (HousePhotoDTO) o;
+        if (housePhotoDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), housePhotoDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "HousePhotoDTO{" +
+            "id=" + getId() +
+            ", image='" + getImage() + "'" +
+            ", mobileLink='" + getMobileLink() + "'" +
+            ", webLink='" + getWebLink() + "'" +
+            ", enabled='" + isEnabled() + "'" +
+            ", createAt='" + getCreateAt() + "'" +
+            ", house=" + getHouseId() +
+            ", createBy=" + getCreateById() +
+            ", createBy='" + getCreateByLogin() + "'" +
+            ", updateBy=" + getUpdateById() +
+            ", updateBy='" + getUpdateByLogin() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/com/landexp/responses/HouseResponse.java
@@ -0,0 +1,275 @@
+package com.landexp.service.dto;
+
+import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Lob;
+import com.landexp.domain.enumeration.UserActionType;
+import com.landexp.domain.enumeration.DirectionType;
+import com.landexp.domain.enumeration.DirectionType;
+import com.landexp.domain.enumeration.LandType;
+import com.landexp.domain.enumeration.SaleType;
+import com.landexp.domain.enumeration.PresentType;
+import com.landexp.domain.enumeration.StatusType;
+
+/**
+ * A DTO for the House entity.
+ */
+public class HouseDTO implements Serializable {
+
+    private Long id;
+
+    private String link;
+
+    @Lob
+    private byte[] avatar;
+    private String avatarContentType;
+
+    private String avatarLink;
+
+    private UserActionType actionType;
+
+    private String address;
+
+    private Float money;
+
+    private Float acreage;
+
+    private Float discount;
+
+    private Integer bathRoom;
+
+    private Integer bedRoom;
+
+    private Boolean parking;
+
+    private LandType landType;
+
+    private LocalDate createAt;
+
+    private LocalDate updateAt;
+
+    private String cityName;
+
+    private String districtName;
+
+    private String wardName;
+
+    private String projectName;
+
+    private String createByLogin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public byte[] getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(byte[] avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getAvatarContentType() {
+        return avatarContentType;
+    }
+
+    public void setAvatarContentType(String avatarContentType) {
+        this.avatarContentType = avatarContentType;
+    }
+
+    public String getAvatarLink() {
+        return avatarLink;
+    }
+
+    public void setAvatarLink(String avatarLink) {
+        this.avatarLink = avatarLink;
+    }
+
+    public UserActionType getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(UserActionType actionType) {
+        this.actionType = actionType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public Float getAcreage() {
+        return acreage;
+    }
+
+    public void setAcreage(Float acreage) {
+        this.acreage = acreage;
+    }
+
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+
+    public Integer getBathRoom() {
+        return bathRoom;
+    }
+
+    public void setBathRoom(Integer bathRoom) {
+        this.bathRoom = bathRoom;
+    }
+
+    public Integer getBedRoom() {
+        return bedRoom;
+    }
+
+    public void setBedRoom(Integer bedRoom) {
+        this.bedRoom = bedRoom;
+    }
+
+    public Boolean isParking() {
+        return parking;
+    }
+
+    public void setParking(Boolean parking) {
+        this.parking = parking;
+    }
+
+    public LandType getLandType() {
+        return landType;
+    }
+
+    public void setLandType(LandType landType) {
+        this.landType = landType;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDate getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDate updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public String getWardName() {
+        return wardName;
+    }
+
+    public void setWardName(String wardName) {
+        this.wardName = wardName;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String landProjectName) {
+        this.projectName = landProjectName;
+    }
+
+    public String getCreateByLogin() {
+        return createByLogin;
+    }
+
+    public void setCreateByLogin(String userLogin) {
+        this.createByLogin = userLogin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        HouseDTO houseDTO = (HouseDTO) o;
+        if (houseDTO.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), houseDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "HouseDTO{" +
+            "id=" + getId() +
+            ", avatar='" + getAvatar() + "'" +
+            ", avatarLink='" + getAvatarLink() + "'" +
+            ", actionType='" + getActionType() + "'" +
+            ", address='" + getAddress() + "'" +
+            ", money=" + getMoney() +
+            ", acreage=" + getAcreage() +
+            ", discount=" + getDiscount() +
+            ", bathRoom=" + getBathRoom() +
+            ", bedRoom=" + getBedRoom() +
+            ", parking='" + isParking() + "'" +
+            ", landType='" + getLandType() + "'" +
+            ", createAt='" + getCreateAt() + "'" +
+            ", updateAt='" + getUpdateAt() + "'" +
+            ", city='" + getCityName() + "'" +
+            ", district='" + getDistrictName() + "'" +
+            ", ward='" + getWardName() + "'" +
+            ", project='" + getProjectName() + "'" +
+            ", createBy='" + getCreateByLogin() + "'" +
+            "}";
+    }
+}
