22819da12ba5e8422065f4c7fb6cd095fd991d93
Tran Anh Tuan <tk1cntt@gmail.com>
2018-07-16 07:22:47 +0700

    Add aws

---
+++ src/main/java/com/landexp/service/S3Service.java
@@ -0,0 +1,52 @@
+package com.landexp.service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.services.s3.transfer.model.UploadResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Service
+public class S3Service {
+
+    private Logger logger = LoggerFactory.getLogger(S3Service.class);
+
+    private String bucket = "images.tinvang.com.vn";
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    private void upload(InputStream inputStream, String s3path) throws AmazonS3Exception {
+        TransferManager transferManager = new TransferManager(amazonS3);
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, s3path, inputStream, new ObjectMetadata());
+        Upload upload = transferManager.upload(putObjectRequest);
+        try {
+            UploadResult result = upload.waitForUploadResult();
+            amazonS3.setObjectAcl(bucket, result.getKey(), CannedAccessControlList.PublicRead);
+        } catch (Exception e) {
+            throw new AmazonS3Exception("Store file to s3 failed", e);
+        }
+    }
+
+    public void save(String s3path, byte[] data) throws AmazonS3Exception {
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(data);
+            upload(bais, s3path);
+        } catch (Exception e) {
+            throw new AmazonS3Exception("Store file to s3 failed", e);
+        }
+    }
+}
