61b3bbbe58580a179cbb18827cca6362415c696d
Tran Anh Tuan <tuanta17@fsoft.com.vn>
2018-07-18 16:28:03 +0700

    Remove unuse

--- src/main/webapp/app/entities/land-project/index.tsx
+++ src/main/webapp/app/entities/land-project/index.tsx
@@ -4,19 +4,15 @@ import { Switch } from 'react-router-dom';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
 import LandProject from './land-project';
-import LandProjectDetail from './land-project-detail';
 import LandProjectUpdate from './land-project-update';
-import LandProjectDeleteDialog from './land-project-delete-dialog';
 
 const Routes = ({ match }) => (
   <>
     <Switch>
       <ErrorBoundaryRoute exact path={`${match.url}/new`} component={LandProjectUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={LandProjectUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={LandProjectDetail} />
       <ErrorBoundaryRoute path={match.url} component={LandProject} />
     </Switch>
-    <ErrorBoundaryRoute path={`${match.url}/:id/delete`} component={LandProjectDeleteDialog} />
   </>
 );
 
--- src/main/webapp/app/entities/payment/index.tsx
+++ src/main/webapp/app/entities/payment/index.tsx
@@ -4,19 +4,15 @@ import { Switch } from 'react-router-dom';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
 import Payment from './payment';
-import PaymentDetail from './payment-detail';
 import PaymentUpdate from './payment-update';
-import PaymentDeleteDialog from './payment-delete-dialog';
 
 const Routes = ({ match }) => (
   <>
     <Switch>
       <ErrorBoundaryRoute exact path={`${match.url}/new`} component={PaymentUpdate} />
       <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={PaymentUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={PaymentDetail} />
       <ErrorBoundaryRoute path={match.url} component={Payment} />
     </Switch>
-    <ErrorBoundaryRoute path={`${match.url}/:id/delete`} component={PaymentDeleteDialog} />
   </>
 );
 
